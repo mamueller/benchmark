b3c0420a58e77e2fdc3a4f6411491176466fe365
Markus Müller <markus.mueller.1.g@googlemail.com>
2013-07-15 11:53:56 +0200

    found an elegant solution for the matSimp Problem

--- benchmark/proto/Coords.py
+++ benchmark/proto/Coords.py
@@ -3,52 +3,52 @@
 from sympy import *
 import unittest 
 class CoordTest(unittest.TestCase):
-    def test_frames_as_instances(s):
-    ## we define a concrete coordinate frame simply as an istance of class coords
-        class Coords(object):
-            def __init__(self,X,scalarSimp):
-                self.X=X
-                self.scalarSimp=scalarSimp
-                def matSimp(Mat):
-                    s=Mat.shape
-                    res=zeros(s)
-                    for i in range (s[0]):
-                        for j in range (s[1]): 
-                            res[i,j]=self.scalarSimp(Mat[i,j])
-                    return(res)
-                self.matSimp=matSimp
+   # def test_frames_as_instances(s):
+   # ## we define a concrete coordinate frame simply as an istance of class coords
+   #     class Coords(object):
+   #         def __init__(self,X,scalarSimp):
+   #             self.X=X
+   #             self.scalarSimp=scalarSimp
+   #             def matSimp(Mat):
+   #                 s=Mat.shape
+   #                 res=zeros(s)
+   #                 for i in range (s[0]):
+   #                     for j in range (s[1]): 
+   #                         res[i,j]=self.scalarSimp(Mat[i,j])
+   #                 return(res)
+   #             self.matSimp=matSimp
 
-            # example for internal use of the method
-            def simplifyMats(self):
-                return(self.matSimp(self.X*eye(3)))
+   #         # example for internal use of the method
+   #         def simplifyMats(self):
+   #             return(self.matSimp(self.X*eye(3)))
 
-        
-            
-     ################### begin checks###############################            
-        x,y,z=symbols("x y z")
-        def testsimp(exp):
-            return(exp.subs({x:y}))
+   #     
+   #         
+   #  ################### begin checks###############################            
+   #     x,y,z=symbols("x y z")
+   #     def testsimp(exp):
+   #         return(exp.subs({x:y}))
 
-        sp=Coords(x,testsimp)
-        s.assertEqual(y*eye(2),sp.matSimp(x*eye(2)))
-        s.assertEqual(y*eye(3),sp.simplifyMats())
-        
-        # additional special properties or even methods 
-        # of the coordinate frame are then 
-        # added later
-        sp.val2=5
-        
-        def f(argthatWillBeBoundToSelf):
-            print("this is f")
-            print(argthatWillBeBoundToSelf.X)
-        import types
-        sp.f=types.MethodType( f, sp )
-        # call it
-        sp.f()
-        
-        
-        
-        
+   #     sp=Coords(x,testsimp)
+   #     s.assertEqual(y*eye(2),sp.matSimp(x*eye(2)))
+   #     s.assertEqual(y*eye(3),sp.simplifyMats())
+   #     
+   #     # additional special properties or even methods 
+   #     # of the coordinate frame are then 
+   #     # added later
+   #     sp.val2=5
+   #     
+   #     def f(argthatWillBeBoundToSelf):
+   #         print("this is f")
+   #         print(argthatWillBeBoundToSelf.X)
+   #     import types
+   #     sp.f=types.MethodType( f, sp )
+   #     # call it
+   #     sp.f()
+   #     
+   #     
+   #     
+   #     
         
         
 
@@ -56,32 +56,29 @@ class CoordTest(unittest.TestCase):
 
 
 ################################## subclassing ###################################
-#    # The idea to use factories for different coordinate frames can be seen as 
-#    # an intermediate way to a subclass (a class is in fact a factory for instances)
-#    def test_coordframes_as_subclasses(s):
-#        class Coords(object):
-#            def __init__(self,scalarSimp):
-#                self.scalarSimp=scalarSimp
-#                def matSimp(Mat):
-#                    s=Mat.shape
-#                    res=zeros(s)
-#                    for i in range (s[0]):
-#                        for j in range (s[1]): 
-#                            res[i,j]=self.scalarSimp(Mat[i,j])
-#                    return(res)
-#                self.matSimp=matSimp
-#
-#        class Spherical(Coords):
-#            def __init__(self):
-#                def sphericalSimp(x):
-#                    return(4*x)
-#                sc=super(Spherical,self)
-#                sc.__init__(sphericalSimp)
-#
-#     ################### begin checks###############################            
-#
-#        sp=Spherical()
-#        s.assertEqual(4*eye(2),sp.matSimp(eye(2)))
+    # The idea to use factories for different coordinate frames can be seen as 
+    # an intermediate way to a subclass (a class is in fact a factory for instances)
+    def test_coordframes_as_subclasses(s):
+        class Coords(object):
+            def scalarSimp(self,exp):
+                pass
+
+            def matSimp(self,Mat):
+                s=Mat.shape
+                res=zeros(s)
+                for i in range (s[0]):
+                    for j in range (s[1]): 
+                        res[i,j]=self.scalarSimp(Mat[i,j])
+                return(res)
+
+        class Spherical(Coords):
+            def scalarSimp(self,x):
+                return(4*x)
+
+     ################### begin checks###############################            
+
+        sp=Spherical()
+        s.assertEqual(4*eye(2),sp.matSimp(eye(2)))
 
 
 
