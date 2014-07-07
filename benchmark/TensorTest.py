3ef4e8e82c99654ffc1e9d0372434d56b6387746
Markus Mueller <mamueller@bgc-jena.mpg.de>
2014-07-07 18:37:11 +0200

    added some comments about base changes

--- benchmark/TensorTest.py
+++ benchmark/TensorTest.py
@@ -913,6 +913,14 @@ class TensorTest(unittest.TestCase):
         
 ###########################################################
     def test_ChangeOfBase(self):
+        # Note that a change of base for linear mappings 
+        # is possible without a scalar product or a metric
+        # While the metric of a base describes its relationship with a certain base
+        # (the cartesian  base vectors ) for a change of base only the description 
+        # of one base as linear combination of the other is needed
+        # Actually the relationship to cartesian coordinates is not 
+        # needed to transform the indices of a vector from one base to another 
+        # A scalar product is however needed for the definition of a roof and cellar basis
         sp=Spherical()
         # as a first example we look at a vector
         v_src=Tensor.Tensor(sp,["src"],{(0,):1})
@@ -927,11 +935,16 @@ class TensorTest(unittest.TestCase):
         self.assertEqual(v_doubleSrc,Tensor.Tensor(sp,["doubleSrc"],{(0,):1./2.}))
         
         # now we look at a projection tensor which in (src) coordinates preserves the first component of a vector.
-        P_r_r=Tensor.Tensor(sp,["src","src"],{(0,0):1})
-        P_dr_r=src2doubleSrc.transform(P_r_r,0)
-        self.assertEqual(P_dr_r,Tensor.Tensor(sp,["doubleSrc","src"],{(0,0):1./2.}))
-        P_dr_dr=src2doubleSrc.transform(P_dr_r,1)
-        self.assertEqual(P_dr_dr,Tensor.Tensor(sp,["doubleSrc","doubleSrc"],{(0,0):1./4.}))
+        P_s_s=Tensor.Tensor(sp,["src","src"],{(0,0):1})
+        # we check that it really does
+        v_src=Tensor.Tensor(sp,["src"],{(0,):1,(1,):2,(2,):3})
+        P_ds_s=src2doubleSrc.transform(P_s_s,0)
+        self.assertEqual(P_ds_s,Tensor.Tensor(sp,["doubleSrc","src"],{(0,0):1./2.}))
+        # now we exchage the other base
+        P_s_ds=src2doubleSrc.transform(P_s_s,1)
+        self.assertEqual(P_ds_s,Tensor.Tensor(sp,["doubleSrc","src"],{(0,0):1./2.}))
+        P_ds_ds=src2doubleSrc.transform(P_ds_s,1)
+        self.assertEqual(P_ds_ds,Tensor.Tensor(sp,["doubleSrc","doubleSrc"],{(0,0):1./4.}))
         
         
         #Pc=sp.roof2cart.transform(P_r,0)
