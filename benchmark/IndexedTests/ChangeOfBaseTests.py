3fb559064110992da8929a72a46c10e9abc1b2db
Markus Mueller <mamueller@bgc-jena.mpg.de>
2015-01-10 02:03:47 +0100

    finished cleaning up the base change tests. Those have still to be extended

--- benchmark/IndexedTests/ChangeOfBaseTests.py
+++ benchmark/IndexedTests/ChangeOfBaseTests.py
@@ -148,10 +148,8 @@ class TestChangeOfBaseGivenByCellarVectorTransform(unittest.TestCase):
 
 
 class TestChangeOfBaseWithMetric(unittest.TestCase):
-    def test_oldRoofToNewRoof(s):
+    def test_oldRoofToNewCellar(s):
         ###################  manual alternative with metric #########################
-        # Note that we do not need a metric in this example (the duality paring is enough)
-        # but can use it if it is available
         # assume that between two cellar bases {B_j,j=1..n} and {b_i ,i=1..n}
         # the following linear mapping exists
         #                       i 
@@ -179,7 +177,7 @@ class TestChangeOfBaseWithMetric(unittest.TestCase):
         # I_A  B  b  v  b    = I_A  B  v  b  b   = I_A   v  B      
         #    j     i  k           j     k  i          j   i       
         # so the components of I_A and A are the same.
-        # we demonstrate this:
+        # there is already a test demonstrating this:
         n=2
         i, j, k        = map(Idx, ['i', 'j','k'])
         m = Manifold('M', n)
@@ -194,68 +192,49 @@ class TestChangeOfBaseWithMetric(unittest.TestCase):
         # new roof base
         Br=OneFormFieldBase(Bc)
         # define the (Identety)transformation
-        IA_bc_Br=TensorIndexSet("IA_bc_Br",[bc,Br])
+        IA=TensorIndexSet("IA",[bc,Br])
         a,b=symbols("a,b")
-        IA_bc_Br[0,0]=a
-        IA_bc_Br[1,1]=b
-        x_c=TensorIndexSet("x",[br])
+        IA[0,0]=a
+        IA[1,1]=b
+        x=TensorIndexSet("x",[br])
         x1,x2=symbols("x1,x2")
-        x_c[0]=x1
-        x_c[1]=x2
+        x[0]=x1
+        x[1]=x2
         # Apply the Identity Tranformation.
-        y_c=TensorIndexSet("y")
-        y_c[j]=IA_bc_Br[i,j]*x_c[i]
-        s.assertEqual(y_c.bases,[Br])
-        s.assertEqual(y_c[0],a*x1)
-        s.assertEqual(y_c[1],b*x2)
+        y=TensorIndexSet("y")
+        y[j]=IA[i,j]*x[i]
+        s.assertEqual(y.bases,[Br])
+        s.assertEqual(y[0],a*x1)
+        s.assertEqual(y[1],b*x2)
 
-        # now we want to transform the roof components of a vector (given wrt  the old cellar base)
-        # to roof components wrt to the new cellar base
-        # We can do this directly or with the help of a metric
+        # now we want to transform the roof components of a vector
+        # to new cellar components 
+        x=TensorIndexSet("x",[bc])
+        x1,x2=symbols("x1,x2")
+        x[0]=x1
+        x[1]=x2
+        
+        # to be able to apply our Identity transformation we have to lower the index of x
+        # We achieve this by the musicial isomorphisms b (for lowering)  
+        # which also can be formally described
+        # as a tensor multiplications with the metric tensor represented wrt the 
+        # the old cellar base vectors
 
         # We define the components of g wrt the roof base
         # therefore the bilinear form is applicable directly to cellar base 
         # vectors and since g induces a scalar product our components
         # should be chosen as the scalar product of cellar base vectors.
         # this is equivalent to the definition in exercise 2.8 on page 39 of Simmonds
-        # nomenclature g_cc means cellar cellar components, which means components with respect to roof roof bases)
-        g_cc=TensorIndexSet("g_cc",[br,br])
+        
+        g=TensorIndexSet("g",[br,br])
         # to make thing visible we chose some symbols
         g00,g11=symbols("g00,g11")
-        g_cc[0,0]=g00
-        g_cc[1,1]=g11
-        P.setMetric("cart",g_cc)
-        x_r=TensorIndexSet("x",[bc])
-        x1,x2=symbols("x1,x2")
-        x_r[0]=x1
-        x_r[1]=x2
-        # to be able to apply our Identity transformation we have to lower the first index and raise the second.
-        # We achieve this by two other transformations which are called the 
-        # musicial isomorphisms # (for raising) and b (for lowering)  which also can be formally described
-        # as a tensor multiplications with the metric tensor represented wrt the appropriate bases
-        # first we transform the roof components x_r back to x_c which we already can handle 
-        # we need the representation of the metric tensor wrt the old cellar base vectors
-        raise(Exception("This Example is incomplete and does not yet test any new features, uncomment the following lines to see missing features "))
-        g_rr=P.getMetric([bc,bc])
-     #   # we allready have the metric wrt the roof base vectors
-     #   mat_cc=zeros(n,n)
-     #   dat=g_cc.data
-     #   for tup in dat.keys():
-     #       mat_cc[tup]=dat[tup]
-     #   mat_rr=mat_cc.inverse_LU()   
-     #   g_rr=TensorIndexSet("IAB",[bc,bc])
-     #   for i in range(n):
-     #       for j in range(n):
-     #           g_rr.data[(i,j)]=mat_rr[i,j]
-     #           
-     #   #g_rr=secondRankTensor(g_cc[i,j].to_matrix().inverseLU(),[bc,bc])
-     #   IA_rr=TensorIndexSet("IA_rr")
-     #   IA_rr[i,j]=IA_bc_Br[i,k]*g_rr[k,j]
-     #   #y_r[j]=IA_br_Bc[i,j]*x[i]
-     #   #self.assertEqual(y.bases,[Br])
-     #   #self.assertEqual(y[0],1./a*x1)
-     #   ##self.assertEqual(y[2],1./b*x2)
-     #   
+        g[0,0]=g00
+        g[1,1]=g11
+        y[j]=IA[i,j]*g[i,k]*x[k]
+        s.assertEqual(y.bases,[Br])
+        raise(Exception("The result should be checked against something. E.g. a hand computed solution"))
+        
      
         
 class TestChangeOfBaseGivenByRoofVectorTransform(unittest.TestCase):
